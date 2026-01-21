eea03eeca42c1f475a0b293b9756d560043a43e2
SwathiGovindaKanchan <swathimendon07@gmail.com>
2026-01-21 18:21:15 +0530

    new program added lab004

--- src/ex_21012026/Lab004.java
+++ src/ex_21012026/Lab004.java
@@ -2,6 +2,6 @@ package ex_21012026;
 
 public class Lab004 {
     public static void main(String[] args) {
-        System.out.println("Hi hello ");
+        System.out.println("Hi hello update");
     }
 }
