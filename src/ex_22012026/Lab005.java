98900afef745cd0461af0e61a1c315342f7b7b72
SwathiGovindaKanchan <swathimendon07@gmail.com>
2026-01-22 15:48:49 +0530

    new program added lab004

---
+++ src/ex_22012026/Lab005.java
@@ -0,0 +1,29 @@
+package ex_22012026;
+
+public class Lab005 {
+    public static void main(String[] args) {
+        //primitive data type
+        //boolean
+        boolean is_married = true;
+        boolean are_you_a_dog= false;
+        System.out.println(is_married);
+        System.out.println(are_you_a_dog);
+
+        //Numeric
+        //integral-byte,int,short,long
+        byte b=100;
+        short s=101;
+        int age=130;
+        long phone_number=9876543210l;
+        //char
+        char grade='A';
+        //floating- float,double
+        float pi=3.14f;
+         double d=118.321;
+
+        //Non-primitive data type
+        //string name="swathi"- number or char or bunch of char
+        String name="Swathi";
+
+    }
+}
