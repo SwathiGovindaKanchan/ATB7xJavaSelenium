bf2526ed5be0fd6904bffc6c6da9a517ecda6add
SwathiGovindaKanchan <swathimendon07@gmail.com>
2026-01-23 15:52:10 +0530

    new program added lab004

---
+++ src/ex_23012026/Lab011.java
@@ -0,0 +1,15 @@
+package ex_23012026;
+
+public class Lab011 {
+    public static void main(String[] args) {
+        byte a=10;
+        byte b=23;
+        int c=a+b;
+        System.out.println(c);
+
+        char a1= 'A';
+        char b1= 'B';
+        String c1=a1+b1;
+        System.out.println(c1);
+    }
+}
